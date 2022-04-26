dca16bbaf6d3e0f6c80f0292a7cc728b785c3bfe
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-04-25 22:32:38 -0300

    authenticate for github

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken" 
 
 /**
  * Receber code(string)
@@ -48,14 +49,14 @@ class AuthenticateUserService {
 
     const {login, id, avatar_url, name} = response.data
 
-    const user = await prismaClient.user.findFirst({ 
+    let user = await prismaClient.user.findFirst({ 
       where: { 
         github_id: id,
       }
     })
 
     if(!user){
-      await prismaClient.user.create({
+      user = await prismaClient.user.create({
         data: {
         github_id: id,
         login,
@@ -65,8 +66,23 @@ class AuthenticateUserService {
       })
     }
 
-    return response.data;
+    const token = sign(
+      {
+        user:{
+         user: user.name,
+          avatar_url: avatar_url,
+          id: user.id 
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn:"1d"
+      }
+    )
+
+    return {token, user};
   }
 }
 
-export { AuthenticateUserService }
+export { AuthenticateUserService };
---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,19 @@
+import  prismaClient from "../prisma";
+
+class CreateMessageService {
+  async execute(text: string, userId:string){
+    const message = await prismaClient.message.create({
+      data : {
+        text,
+        userId,
+      },
+      include: {
+        user : true,
+      },      
+    });
+
+    return message;
+  }
+}
+
+export { CreateMessageService };
