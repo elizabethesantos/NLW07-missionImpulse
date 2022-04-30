32016d27feebd11808356f94100882d0805f2d01
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-04-29 23:38:07 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import './App.modules.css'
 
 export function App() {
   return (
-    <main className={contentWrapper">
+    <main className={contentWrapper}>
       <h1>Hello World</h1>
     </main>
   )
